29e256a082031b8314488ae68e890038568efb67
mzamoun <mourad.zamoun@gmail.com>
2026-02-16 22:10:38 +0100

    export_cra_with_spring_batch

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   today = new Date();
-	dateCommit = "Last Commit : 2026-02-16 22:10:20"
+	dateCommit = "Last Commit : 2026-02-16 22:10:37"
   dateFooter = "";
 
   constructor() { }
