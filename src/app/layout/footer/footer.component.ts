a934610c472f075c4890bdd52d81cc6c23d644ee
mzamoun <mourad.zamoun@gmail.com>
2026-07-03 10:55:56 +0200

    debug_month_of_cra_3

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -10,7 +10,7 @@ import { UtilsService } from 'src/app/service/utils.service';
 })
 export class FooterComponent implements OnInit {
   today = new Date();
-	dateCommit = "Last Commit : 2026-07-03 10:14:50"
+	dateCommit = "Last Commit : 2026-07-03 10:53:02"
   dateFooter = "";
 
   constructor(private logger: LoggerService, public utils: UtilsService) { }
