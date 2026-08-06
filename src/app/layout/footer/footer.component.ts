dafe1e6664caa5116cca3282892149cfb4725ddc
mzamoun <mourad.zamoun@gmail.com>
2026-08-07 00:03:55 +0200

    ClassesMappers_corrections_erreurs_Nizar_etc_3

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -11,7 +11,7 @@ import { environment } from 'src/environments/environment';
 })
 export class FooterComponent implements OnInit {
   // today = new Date();
-	dateCommitFront = "2026-08-06 23:44:08"
+	dateCommitFront = "2026-08-06 23:56:38"
   dateCommitServer = ""
 
   // dateFooter = "";
